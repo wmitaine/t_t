5938aaf52588f341781c2cd2b712fd797c4541fb
William Mitaine <william.mitaine@gmail.com>
2015-10-06 22:51:36 +0200

    ajout joueur 2

--- collection/player.js
+++ collection/player.js
@@ -2,19 +2,16 @@ Players = new Mongo.Collection("players");
 
 Meteor.methods({
 	createPlayer: function(player){
-		var existedPlayer = Players.findOne({name : player.name});
+		var existedPlayer = Players.findOne({"name":player.name});
 
-		console.log(existedPlayer._id);
-		if(existedPlayer._id){
+		if(existedPlayer){
 			console.log(player.name + " existe déjà.");
 			return false;
 		}else{
-			newPlayer = Players.insert({name: player.name, createdAt : Date.now()}, function(error, result){});
-			if(newPlayer._id){
-				console.log(newPlayer.name + " est bien crée.");
+			newPlayer = Players.insert({"name": player.name, "createdAt" : Date.now()}, function(error, result){});
+			if(newPlayer){
 				return true;
 			}else{
-				console.log("Une erreur est survenue à la création du joueur");
 				return false;
 			}
 		}
